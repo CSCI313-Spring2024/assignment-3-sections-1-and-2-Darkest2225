32a9bf81ec8d7e5784b458dbbfc93f968b85957e
Darkest2225 <dwschroeder01@gmail.com>
2025-03-18 17:05:27 -0500

    Integrated details page into the application

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [CommonModule],
+  imports: [RouterModule, RouterLink, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
