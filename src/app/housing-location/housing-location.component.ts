8f5b9401e0399829122ae41dc46e1a330c66110d
Darkest2225 <dwschroeder01@gmail.com>
2025-03-17 21:52:32 -0500

    "Added Inputs to the HousingLocationComponent"

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
+  
+  @Input() housingLocation!: HousingLocation;
 
 }
